bd7895b4cfacca572ff9d5641cca97f95021bfa0
fredlim <hungwai.lim@gmail.com>
2024-07-22 16:09:56 +0800

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'website': 'https://www.odoo.com',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'license': 'LGPL-3'
+}
---
+++ models/estate_property.py
@@ -0,0 +1,29 @@
+from dateutil.relativedelta import relativedelta
+from odoo import models, fields
+
+
+class Property(models.Model):
+    _name = 'estate.property'
+    _description = 'Property to sale'
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('N','North'), ('S', 'South'), ('E', 'East'), ('W', 'West')]
+    )
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string = 'Status',
+        selection=[('1', 'New'), ('2', 'Offer Received'), ('3', 'Offer Accepted'), ('4', 'Sold'), ('5', 'Canceled')],
+        default='1'
+    )
