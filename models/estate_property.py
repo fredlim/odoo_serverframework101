e4c9e1b2792f02f629de95c02de5dc077f3e742b
fredlim <hungwai.lim@gmail.com>
2024-07-22 17:56:14 +0800

    Fixed the type in estate_property_views.xml

--- models/estate_property.py
+++ models/estate_property.py
@@ -3,8 +3,8 @@ from odoo import models, fields
 
 
 class Property(models.Model):
-    _name = 'estate.property'
-    _description = 'Property to sale'
+    _name = "estate.property"
+    _description = "Property to sale"
 
     name = fields.Char(required=True)
     description = fields.Text()
@@ -12,18 +12,18 @@ class Property(models.Model):
     date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
+    bedrooms = fields.Integer(default=3)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection=[('N','North'), ('S', 'South'), ('E', 'East'), ('W', 'West')]
+        selection=[("N","North"), ("S","South"), ("E","East"), ("W","West")]
     )
-    active = fields.Boolean('Active', default=True)
+    active = fields.Boolean("Active", default=True)
     state = fields.Selection(
-        string = 'Status',
-        selection=[('1', 'New'), ('2', 'Offer Received'), ('3', 'Offer Accepted'), ('4', 'Sold'), ('5', 'Canceled')],
-        default='1'
+        string = "Status",
+        selection=[("1","New"), ("2","Offer Received"), ("3","Offer Accepted"), ("4","Sold"), ("5","Canceled")],
+        default="1"
     )
